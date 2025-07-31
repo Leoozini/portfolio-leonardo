028da4419e83357a924141f5e2ba9db57517a53a
Leonardo Dos Santos Souza <contatoleonardo13@hotmail.com>
2025-07-31 02:17:28 -0300

    Limpa rastreamento errado e ajusta .gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "", // Use "" para rodar localmente. Quando for publicar no GitHub Pages, troque para "/portfolio-leonardo/"
+  base: "/portfolio-leonardo/", // <- TROQUEI AQUI para o GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {

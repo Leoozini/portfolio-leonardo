91521b93747969d9a326eea95c35d3bded3690fd
Leonardo Dos Santos Souza <contatoleonardo13@hotmail.com>
2025-07-31 02:53:33 -0300

    :rocket: Commit Inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
 
